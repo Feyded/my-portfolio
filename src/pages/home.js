a69168ba8a101e1d95e3f013c4e631af69e8f5d0
Feyded <126765808+Feyded@users.noreply.github.com>
2024-07-10 21:11:25 +0800

    change image from jpg to png

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import personImage from "../assets/images/home/person.jpg";
+import personImage from "../assets/images/home/person.png";
 import Swal from "sweetalert2";
 import "../assets/css/home.css";
 
