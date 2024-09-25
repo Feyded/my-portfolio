82b1e34e08f591f96a319ed9a2fe681a1345ff1a
Feyded <126765808+Feyded@users.noreply.github.com>
2024-09-25 09:58:07 +0800

    add new works

--- src/pages/works.js
+++ src/pages/works.js
@@ -1,7 +1,7 @@
 import React from "react";
 import newWork1 from "../assets/images/works/newWork1.png";
 import newWork2 from "../assets/images/works/newWork2.png";
-import work12 from "../assets/images/works/work12.png";
+import newWork3 from "../assets/images/works/newWork3.png";
 import "../assets/css/works.css";
 import { ArrowRight } from "lucide-react";
 
@@ -28,16 +28,10 @@ const works = [
     buttonUrl: "https://github.com/Feyded/inventory-management",
   },
   {
-    imageUrl: work12,
-    techstack: [
-      "React",
-      "Nextjs",
-      "Tailwindcss",
-      "Framer Motion",
-      "Redux",
-    ],
+    imageUrl: newWork3,
+    techstack: ["React", "Nextjs", "Tailwindcss", "Framer Motion"],
     buttonLabel: "View Demo",
-    buttonUrl: "http://101production.infinityfreeapp.com",
+    buttonUrl: "https://myprojects-three.vercel.app/mock-design",
   },
 ];
 
@@ -70,16 +64,16 @@ export function Works() {
                   ))}
                 </div>
                 <br />
-                  <a
-                    href={work.buttonUrl}
-                    className="group inline-flex justify-center mt-auto items-center px-3 gap-1 py-2 text-sm font-medium text-center text-white bg-orange-500 rounded-lg hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
-                  >
-                    {work.buttonLabel}
-                    <ArrowRight
-                      className="animate-bounceX group-hover:animate-none"
-                      size={20}
-                    />
-                  </a>
+                <a
+                  href={work.buttonUrl}
+                  className="group inline-flex justify-center mt-auto items-center px-3 gap-1 py-2 text-sm font-medium text-center text-white bg-orange-500 rounded-lg hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+                >
+                  {work.buttonLabel}
+                  <ArrowRight
+                    className="animate-bounceX group-hover:animate-none"
+                    size={20}
+                  />
+                </a>
               </div>
             </div>
           ))}
